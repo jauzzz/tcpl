4eea4cc9e17523474015dbe76f3e8a458a8a136e
jauzzz <18814128016@163.com>
2020-09-24 18:14:02 +0800

    ch1

--- ch1/exercise/ex21.c
+++ ch1/exercise/ex21.c
@@ -5,6 +5,10 @@
  * Use the same tab stops as for detab.
  * When either a tab or a single blank would suffice to reach a tab stop,
  * which should be given preference?
+ * 
+ * 编写程序entab，将空格串替换为最少数量的制表符和空格，但要保持单词之间的间隔不变。
+ * 假设制表符终止位的位置与练习 1-20 的 detab 程序的情况相同。
+ * 当使用一个制表符或者一个空格都可以到达下一个制表符终止位时，选用哪一种替换字符比较好?
  */
 
 #include <stdio.h>
